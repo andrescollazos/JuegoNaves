16ac69cddb0e04eec8a32cdb2a88e152209a335f
andrescollazos <pipecollazos1@gmail.com>
2016-04-21 17:12:01 -0500

    Se crean enemigos, que se mueven

--- naves.py
+++ naves.py
@@ -1,12 +1,28 @@
-import pygame 
+import pygame
 from pygame.locals import *
 import time
 import sys
 import random
 
-ANCHO=600
-ALTO=400
-BLANCO=(255,255,255)
+ANCHO = 800
+ALTO = 600
+BLANCO = (255,255,255)
+
+# Imagenes de enemigos del primer nivel
+enemys1 = [
+		"images/Enemies/enemyBlack1.png",
+		"images/Enemies/enemyBlue1.png",
+		"images/Enemies/enemyGreen1.png",
+		"images/Enemies/enemyRed1.png"
+]
+
+# Imangenes de enemigos del segundo nivel
+enemys2 = [
+		"images/Enemies/enemyBlack2.png",
+		"images/Enemies/enemyBlue2.png",
+		"images/Enemies/enemyGreen2.png",
+		"images/Enemies/enemyRed2.png"
+]
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, imagen):
@@ -35,6 +51,7 @@ class Enemy(pygame.sprite.Sprite):
 		self.image = pygame.image.load(imagen).convert_alpha() #Cargar imagen
 		self.rect = self.image.get_rect()
 		self.lives = 2 # Vidas del enemigo
+		self.direccion = 0 # Sentido del enemigo
 
 	# Modificar posicion del enemigo
 	def set_pos(self, pos):
@@ -43,7 +60,7 @@ class Enemy(pygame.sprite.Sprite):
 
 	# Actualizar el estado del enemigo
 	def update(self):
-		pass
+		self.rect.y += 1
 
 	# Realizar disparo
 	def shot(self):
@@ -60,14 +77,33 @@ class Bullet(pygame.sprite.Sprite):
 		pass
 
 
+def crearEnemigo():
+	# Seleccionar un enemigo aleatorio
+	e = random.randrange(0, len(enemys1))
+	enemigo = Enemy(enemys1[e])
+	enemigo.rect.x = random.randrange(ANCHO)
+	enemigo.rect.y = random.randrange(-200, -20)
+	return enemigo
+
+
 if __name__ == '__main__':
 	pygame.init()
-	screen = pygame.display.set_mode([ANCHO,ALTO])	
+	screen = pygame.display.set_mode([ANCHO,ALTO])
+	# Cargar imagen de fonod
 	background = pygame.image.load("images/background.jpg").convert_alpha()
 
+	# Crear un jugador
 	jugador = Player("images/playerShip1_red.png")
-
+	# Lista que va a contener todos los elementos
 	ls_todos = pygame.sprite.Group()
+	# Lista que contiene todos los enemigos
+	ls_enemigo = pygame.sprite.Group()
+
+	# Crear enemigos
+	for i in range(10):
+		e = crearEnemigo()
+		ls_enemigo.add(e)
+		ls_todos.add(e)
 	ls_todos.add(jugador)
 
 	pygame.mouse.set_visible(False)
@@ -81,9 +117,10 @@ if __name__ == '__main__':
 			if event.type == pygame.QUIT:
 				terminar = True
 
-		jugador.set_pos(pos)		
+		jugador.set_pos(pos)
 		screen.blit(background,(0,0))
 
+		ls_todos.update()
 		ls_todos.draw(screen)
 		reloj.tick(60)
 		pygame.display.flip()
